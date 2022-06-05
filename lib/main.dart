deddda99e5d8840bca05ae4588c051144732926d
yerimJu <yr2675@gmail.com>
2022-06-06 01:30:42 +0900

    Add image list UI by gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
+  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  static const showGrid = true; // Set to false to show ListView
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Animal Analyzer',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Animal Analyzer Demo'),
+        ),
+        body: const MyStatelessWidget(),
+        // body: Center(child: showGrid ? _buildGrid() : _buildList()),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count, (i) => Container(child: Image.asset('images/pic$i.jpg')));
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+            child: GridView.builder(
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
+          itemCount: 1,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1, //1 개의 행에 보여줄 item 개수
+            childAspectRatio: 1 / 2, //item 의 가로 1, 세로 2 의 비율
+            mainAxisSpacing: 20, //수평 Padding
+            crossAxisSpacing: 20, //수직 Padding
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return GridView.extent(
+                maxCrossAxisExtent: 300,
+                padding: const EdgeInsets.all(4),
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                children: _buildGridTileList(10));
+          },
+        )),
+        OutlinedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          child: const Text('Click Me'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
+
+// // The images are saved with names pic0.jpg, pic1.jpg...pic29.jpg.
+// // The List.generate() constructor allows an easy way to create
+// // a list when objects have a predictable naming pattern.
+// List<Container> _buildGridTileList(int count) => List.generate(
+//     count, (i) => Container(child: Image.asset('images/pic$i.jpg')));
+// // #enddocregion grid
+
+
+// // #docregion grid
+// Widget _buildGrid() => GridView.extent(
+//     maxCrossAxisExtent: 300,
+//     padding: const EdgeInsets.all(4),
+//     mainAxisSpacing: 4,
+//     crossAxisSpacing: 4,
+//     children: _buildGridTileList(10));
+
+// // #docregion list
+// Widget _buildList() {
+//   return ListView(
+//     children: [
+//       _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+//       _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+//       _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+//       _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+//       _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+//           Icons.theaters),
+//       _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+//       const Divider(),
+//       _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+//       _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+//       _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+//       _tile('La Ciccia', '291 30th St', Icons.restaurant),
+//     ],
+//   );
+// }
+
+// ListTile _tile(String title, String subtitle, IconData icon) {
+//   return ListTile(
+//     title: Text(title,
+//         style: const TextStyle(
+//           fontWeight: FontWeight.w500,
+//           fontSize: 20,
+//         )),
+//     subtitle: Text(subtitle),
+//     leading: Icon(
+//       icon,
+//       color: Colors.blue[500],
+//     ),
+//   );
+// }
+// // #enddocregion list
